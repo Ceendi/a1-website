eaa72f90dd23e54de387ad4b42227bd5fd8cb0e7
Ceendi <ceendek@gmail.com>
2025-05-13 16:35:36 +0200

    add ClientWrapper

---
+++ src/app/components/ClientWrapper/index.tsx
@@ -0,0 +1,14 @@
+// src/app/components/ClientWrapper.tsx
+"use client";
+
+import dynamic from "next/dynamic";
+
+const ClientLayout = dynamic(() => import("../ClientLayout"), { ssr: false });
+
+export default function ClientWrapper({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <ClientLayout>{children}</ClientLayout>;
+}
