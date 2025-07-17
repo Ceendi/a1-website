a0eb273d26e5a9fe508cc449743ad9384df84ae3
Ceendi <ceendek@gmail.com>
2025-07-17 20:51:02 +0200

    some changes in projects

--- next/src/app/[locale]/projects/page.tsx
+++ next/src/app/[locale]/projects/page.tsx
@@ -97,13 +97,13 @@ export default async function Projects() {
         className={`min-h-screen w-full lg:ml-32 lg:mr-32`}
         style={{ fontFamily: '"Viner Hand ITC", cursive' }}
       >
-        <div className="text-center mb-2 mt-8">
-          <h1 className="text-7xl font-extralight">
-            Nasze{" "}
-            <span className="text-red-600 drop-shadow-[0_1px_1px_rgba(0,0,0,0.8)]">
-              Projekty
-            </span>
-          </h1>
+        <div className={`mt-8 mb-8 ${montserrat.className} font-bold`}>
+          <div className="text-3xl tracking-[18px] font-light ml-[65]">
+            pracownia architektoniczna
+          </div>
+          <div className="text-8xl ml-[95] mt-[30]">
+            A<span className="text-red-600">1</span> Sp. z o.o.
+          </div>
         </div>
 
         <div className={`grid gap-8`}>
@@ -119,52 +119,65 @@ export default async function Projects() {
                     isEven ? "lg:flex-row" : "lg:flex-row-reverse"
                   } gap-8 w-full`}
                 >
-                  <div className="relative w-full lg:w-[75%] transition-transform duration-300 ease-in-out hover:scale-[1.02]">
+                  <div className="relative w-full lg:w-[75%] overflow-hidden group rounded-[30px]">
                     {project.mainImage && (
                       <Link
                         href="/projects/[id]"
                         as={`/projects/${project.slug}`}
                       >
                         <ViewTransition name={`project-image-${project.slug}`}>
-                          <Image
-                            priority={true}
-                            width={2000}
-                            height={1000}
-                            src={
-                              project.mainImage.url.startsWith("http")
-                                ? project.mainImage.url
-                                : `${config.api}${project.mainImage.url}`
-                            }
-                            alt={
-                              project.mainImage.alternativeText ||
-                              "Main project Image"
-                            }
-                            className="rounded-[30px] w-full h-auto"
-                          />
+                          <div className="relative w-full h-auto">
+                            <Image
+                              priority={true}
+                              width={2000}
+                              height={1000}
+                              src={
+                                project.mainImage.url.startsWith("http")
+                                  ? project.mainImage.url
+                                  : `${config.api}${project.mainImage.url}`
+                              }
+                              alt={
+                                project.mainImage.alternativeText ||
+                                "Main project Image"
+                              }
+                              className="w-full h-auto transition-transform duration-300 ease-in-out group-hover:scale-105"
+                            />
+                            <div className="absolute top-0 left-0 right-0 bottom-0 pointer-events-none">
+                              <div
+                                className="absolute"
+                                style={{
+                                  top: "20px",
+                                  left: "20px",
+                                  right: "20px",
+                                  bottom: "20px",
+                                  border: "1px solid rgba(255,255,255,0.6)",
+                                  borderRadius: "10px",
+                                  opacity: 0.8,
+                                }}
+                              ></div>
+                            </div>
+                          </div>
                         </ViewTransition>
                       </Link>
                     )}
 
-                    <div
-                      className={`absolute top-[0.7px] ${
-                        isEven ? "left-[0.7px]" : "right-[0.7px]"
-                      }`}
+                    <h3
+                      className={`text-[clamp(1rem,2vw,4rem)] font-bold absolute top-[0px]
+                            bg-black/20 backdrop-blur-[6px] border border-white/60
+                            rounded-[10px] p-3 m-5 text-white ${
+                              montserrat.className
+                            } 
+                            ${isEven ? "left-[0px]" : "right-[0px]"}`}
                     >
-                      <h3
-                        className={`text-[clamp(1rem,2vw,4rem)] font-bold
-                            bg-black/20 backdrop-blur-[6px] border border-white/50
-                            rounded-[10px] p-3 m-5 text-white ${montserrat.className}`}
-                      >
-                        {project.title.toUpperCase()}
-                      </h3>
-                    </div>
+                      {project.title.toUpperCase()}
+                    </h3>
 
                     <div
                       className={`
                             lg:absolute lg:bottom-0 p-3 m-5 ${
                               isEven ? "lg:right-0" : "lg:left-0"
                             } lg:text-[clamp(0.5rem,0.9vw,2rem)]
-                            lg:bg-black/20  backdrop-blur-[6px] border border-white/50
+                            lg:bg-black/20  backdrop-blur-[6px] border border-white/60
                             p-4 rounded-[10px] text-black lg:text-white 
                             max-w-full lg:max-w-[30%] ${montserrat.className}`}
                     >
